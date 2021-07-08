1c208c230b7714ca062c64535a31b533640535d8
Guilherme Golfetto <guilherme.golfetto@hotmail.com>
2021-07-08 00:24:50 -0300

    Initial Commit

---
+++ Exemplo Lottie Carthage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Exemplo Lottie Carthage
+//
+//  Created by Guilherme Golfetto on 08/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
